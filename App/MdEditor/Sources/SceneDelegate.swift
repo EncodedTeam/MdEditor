5bd40325699a850c05446e2371360e311818015a
dutysniper <natarov.constantin@yandex.ru>
2024-01-12 14:45:18 +0300

    add full todolist

--- App/MdEditor/Sources/SceneDelegate.swift
+++ App/MdEditor/Sources/SceneDelegate.swift
@@ -3,19 +3,24 @@
 //
 
 import UIKit
+import TaskManagerPackage
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
 	var window: UIWindow?
-	
+	private var appCoordinator: ICoordinator! // swiftlint:disable:this implicitly_unwrapped_optional
+
 	func scene(
 		_ scene: UIScene,
 		willConnectTo session: UISceneSession,
 		options connectionOptions: UIScene.ConnectionOptions
 	) {
-		guard let windowScene = (scene as? UIWindowScene) else { return }
-		let window = UIWindow(windowScene: windowScene)
-		window.rootViewController = ViewController()
-		window.makeKeyAndVisible()
+		guard let scene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: scene)
+
+		appCoordinator = AppCoordinator(window: window)
+		appCoordinator.start()
+
 		self.window = window
 	}
 }
---
+++ App/MdEditor/Sources/TodoList/Coordinators/MainCoordinator.swift
@@ -0,0 +1,55 @@
+//
+//  MainCoordinator.swift
+//  TodoList
+//
+
+import UIKit
+import TaskManagerPackage
+
+final class MainCoordinator: BaseCoordinator {
+
+	// MARK: - Dependencies
+
+	private let tabBarController: UITabBarController
+	private let taskManager = TaskManager()
+
+	// MARK: - Private properties
+
+	private let pages: [TabbarPage] = TabbarPage.allTabbarPages
+
+	// MARK: - Initialization
+	
+	init(tabBarController: UITabBarController) {
+		self.tabBarController = tabBarController
+	}
+
+	// MARK: - Internal methods
+
+	override func start() {
+		tabBarController.viewControllers?.enumerated().forEach { item in
+			guard let controller = item.element as? UINavigationController else { return }
+			runFlowByIndex(item.offset, on: controller)
+		}
+	}
+}
+
+// MARK: - run Flows -
+private extension MainCoordinator {
+	func runFlowByIndex(_ index: Int, on controller: UINavigationController) {
+		let coordinator: ICoordinator
+		switch pages[index] {
+		case .todoList:
+			coordinator = TodoListCoordinator(
+				navigationController: controller,
+				taskManager: taskManager
+			)
+		case .chart:
+			coordinator = ChartsCoordinator(
+				navigationController: controller,
+				taskManager: taskManager
+			)
+		}
+		addDependency(coordinator)
+		coordinator.start()
+	}
+}
