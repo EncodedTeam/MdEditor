31f68c903656c93b538a1ed1f75e4808e382c13b
Konstantin Abroskin <64919648+aksilont@users.noreply.github.com>
2024-01-29 21:02:23 +0300

    MDED-29 Добавлен Stub для Login (#32)

--- App/MdEditorUITests/Scenes/Login/LoginUITests.swift
+++ App/MdEditorUITests/Scenes/Login/LoginUITests.swift
@@ -11,12 +11,14 @@ final class LoginUITests: XCTestCase {
 		let app = XCUIApplication()
 		let loginScreen = LoginScreenObject(app: app)
 		let todoScreen = TodoListScreenObject(app: app)
+		app.launchArguments.append(LaunchArguments.isUItesting)
+		app.launchArguments.append(contentsOf: LaunchArguments.appLanguage)
 		app.launch()
 
 		loginScreen
 			.isLoginScreen()
-			.set(login: "Admin")
-			.set(password: "pa$$32!")
+			.set(login: "Login")
+			.set(password: "Password")
 			.login()
 
 		todoScreen
@@ -26,12 +28,14 @@ final class LoginUITests: XCTestCase {
 	func test_login_withInValidCred_mustBeShowAlert() {
 		let app = XCUIApplication()
 		let loginScreen = LoginScreenObject(app: app)
+		app.launchArguments.append(LaunchArguments.isUItesting)
+		app.launchArguments.append(contentsOf: LaunchArguments.appLanguage)
 		app.launch()
 
 		loginScreen
 			.isLoginScreen()
-			.set(password: "user")
-			.set(login: "wrongPass")
+			.set(login: "wrongLogin")
+			.set(password: "wrongPass")
 			.login()
 			.closeAlert()
 	}
